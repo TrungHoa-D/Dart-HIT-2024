5cd0e1efa53e910db387913debd91b29ad7f1457
TrungHoa-D <trunghoa2k4@gmail.com>
2024-03-15 17:43:43 +0700

    fix leap year

--- Week1/Ex1.dart
+++ Week1/Ex1.dart
@@ -1,7 +1,13 @@
 bool leapYear(int year) {
-  if(year % 4== 0) return true;
+  if(year % 4== 0) {
+    if(year % 100 ==0) {
+        if(year % 400 == 0) return true;
+        else return false;
+    }
+    else return true;
+  }
   return false;
 }
 void main(){
-  print(leapYear(2024));
+  print(leapYear(2400));
 }
